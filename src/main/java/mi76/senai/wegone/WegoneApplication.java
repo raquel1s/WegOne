5df9199f82eca0745e90af226c0bcba612edc39f
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-05-06 19:31:11 -0300

    Primeiro commit

---
+++ src/main/java/mi76/senai/wegone/WegoneApplication.java
@@ -0,0 +1,13 @@
+package mi76.senai.wegone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mi76/senai/wegone/WegoneApplicationTests.java
@@ -0,0 +1,13 @@
+package mi76.senai.wegone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
