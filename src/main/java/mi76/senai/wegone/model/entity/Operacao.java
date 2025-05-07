9af6ca3ac31f21e73c420ebef53abc7b1eba5131
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-05-06 21:29:31 -0300

    arrumando

--- src/main/java/mi76/senai/wegone/model/entity/Operacao.java
+++ src/main/java/mi76/senai/wegone/model/entity/Operacao.java
@@ -1,10 +1,13 @@
 package mi76.senai.wegone.model.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Operacao {
 
     @Id
